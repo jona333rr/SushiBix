413035703add82191819561cad10bb6c94922cf8
jona333rr <jona333r@edu.sde.dk>
2024-08-28 10:08:49 +0200

    Full project (Fixed errors)

--- components/antalField.tsx
+++ components/antalField.tsx
@@ -7,7 +7,7 @@ let antal: 0;
 export default function AntalField() {
   const [antal, setAntal] = useState(0);
 
-  const handleChange = (e) => {
+  const handleChange = (e : any) => {
     const inputValue = e.target.value;
 
     setAntal(inputValue);
